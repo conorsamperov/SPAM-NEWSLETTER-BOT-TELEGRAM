d4a084833aceca519dfa416f8373fe920f31e798
CONOR <conorwoodi777@gmail.com>
2026-01-21 21:36:09 +0300

    Update chat_id.py

--- chat_id.py
+++ chat_id.py
@@ -1,9 +1,11 @@
 # Developer: CONOR (@conorxz) | Dear Talent: @deartalent
 
+## Ниже добавлять чаты. Если у вас канал, группа, то добавляйте перед его ID "-"
 CHATS = [
-    -5108343154, # Чат
-    8210319781,# Мой лс
+    -123123123123, # Чат
+    123123123123, # Лс
 ]
 
 def get_chats():
     return CHATS.copy()
+
